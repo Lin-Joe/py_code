101db6d0c66c0697ce3edb2014a0d55be72194ab
Lin-Joe <403546446@qq.com>
2022-05-20 00:13:38 +0800

    Changes

--- 12334.py
+++ 12334.py
@@ -1,1 +1,67 @@
-djhjfhwfw
+import time
+from Transbot_Lib import Transbot
+from ipywidgets import interact
+import ipywidgets as widgets
+bot = Transbot()
+bot.create_receive_threading()
+
+enable = True
+bot.set_auto_report_state(enable, forever=False)
+enable=False
+bot.clear_auto_report_data()
+
+bot.set_car_motion(0,0)
+
+x = 100
+y = 200
+z = 100
+
+while(1):
+    a = input("请输入")
+
+    print(a)
+    if a=="s":
+        bot.set_car_motion(-1, 0)
+    elif a=="w":
+        bot.set_car_motion(1, 0)
+    elif a=="a":
+        bot.set_car_motion(0, 1)
+    elif a == "d":
+        bot.set_car_motion(0, -1)
+
+    elif a=="u":
+        x = x-10
+        bot.set_uart_servo_angle(7,x,800)
+    elif a=="j":
+        x = x+10
+        bot.set_uart_servo_angle(7,x,800)
+
+    elif a == "i":
+        y = y - 10
+        bot.set_uart_servo_angle(8, y, 800)
+    elif a == "k":
+        y = y + 10
+        bot.set_uart_servo_angle(8, y, 800)
+
+    elif a=="o":
+        z = z-10
+        bot.set_uart_servo_angle(9,z,400)
+    elif a=="l":
+        z = z+10
+        bot.set_uart_servo_angle(9,z,400)
+
+    elif a == " ":
+        bot.set_car_motion(0, 0)
+
+while True:
+    i=input("getkey")
+    if i==“q”:
+        line =-line
+        bot. set_car_motion(line/100.0, angular/100.0)
+    elif i=="z":
+        angular= -angular
+        bot. set_car_motion(line/100, angular/100.0)
+    elif i=="w":
+        line =line*9.9
+        angular = angular*9.9
+        bot. set_car_motion(line/100, angular/100)
--- 图像处理.py
+++ 图像处理.py
@@ -1,47 +1,34 @@
 
+#反色、旋转、膨胀、设定不同阈值
 
-
-#反色
-import cv2
-src=cv2.imread("C:\\Users\\wei\\Pictures\\Saved Pictures\\apple.jpg",1)
-print(src.shape)
-Img=255-src
-print(Img.shape)
-cv2.imshow("Img",Img)
-cv2.imshow("src",src)
-cv2.waitKey(0)
-#旋转
+import cv2 as cv
 import os
 from PIL import Image
-img = Image.open('img/1.jpg')
-img = img.transpose(Image.ROTATE_90)  # 将图片旋转90度
-#img = img.transpose(Image.ROTATE_180)  # 将图片旋转180度
-#img = img.transpose(Image.ROTATE_270)  # 将图片旋转270度
-img.show("img/rotateImg.png")
-#img.save("img/rotateImg.png")
+
+#读取图片
+src=cv.imread("face.jpg",1)
+gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+#反色
+inve= 255 - src
+cv.imwrite("fanse.jpg", inve)
+
+#旋转
+rota = Image.open("facep.png")
+rota = rota.transpose(Image.ROTATE_90)  # 将图片旋转90度
+rota.save("xuanzhuan.png", quality=95, subsampling=0)
 
 #膨胀
-import cv2 as cv
-
-def dilate_demo(image):
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    ret, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
-    cv.imshow("binary", binary)
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
-    dst = cv.dilate(binary, kernel)
-    cv.imshow("dilate", dst)
-
-src = cv.imread("shufa.jpg")
-cv.imshow("shufa", src)
-# erode_demo(src)
-dilate_demo(src)
-cv.waitKey(0)
-cv.destroyAllWindows()
+ret, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+dst = cv.dilate(binary, kernel)
+cv.imwrite("pengzhang.jpg",dst)
 
 #阈值化处理
-img=cv2.imread("C:/picture/dog.jpg")
-GrayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-r,b=cv2.threshold(GrayImage,127,255,cv2.THRESH_BINARY)
-print(r)
-cv2.imshow("img",img)
-cv2.imshow("result",b)
+r,b=cv.threshold(gray,127,255,cv.THRESH_BINARY)
+cv.imwrite("yuzhi127.jpg",b)
+r,bb=cv.threshold(gray,200,255,cv.THRESH_BINARY)
+cv.imwrite("yuzhi200.jpg",bb)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
